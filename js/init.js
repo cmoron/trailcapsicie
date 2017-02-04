ca7f1c57ab6e79ef7c5f6120cb37d44df03ff741
cmoron <cyril.moron@gmail.com>
2017-02-04 23:19:49 +0100

    initial commit

---
+++ js/init.js
@@ -0,0 +1,30 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+    $(".bxslider").bxSlider({
+        auto: true,
+        autoControls: true
+    });
+    $(".fancybox").fancybox({
+        helpers: {
+            title : {
+                type : 'inside'
+            }
+        }
+    });
+});
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.8";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
